383492e6c0f79ed1fb5c8e8990987722169ee91b
irina-p-d <irinapetrovap@gmail.com>
2022-02-10 10:18:55 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.jpg">
+  <p>Comprueba si puedes conducir</p>
+  <check-age/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CheckAge from './components/CheckAge.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CheckAge
+    
   }
 }
 </script>
---
+++ src/components/CheckAge.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="form-age">
+   
+    <div class="d-flex">
+                <input
+                    v-bind:type="passwordType" 
+                    class="form-control" 
+                    placeholder="Introduce tu edad"
+                >
+                <button 
+                    class="btn btn-primary"
+                    @click="showPassword"
+                >{{showPasswordText}}</button>    
+            </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CheckAge',
+  props: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.form-age {
+  width: 50%;
+  margin: 0 auto;
+}
+</style>
