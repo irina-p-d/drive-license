09af9836b1f165e92176bf8433f575454305de51
irina-p-d <irinapetrovap@gmail.com>
2022-02-10 11:43:43 +0100

    project vue created

--- src/components/CheckAge.vue
+++ src/components/CheckAge.vue
@@ -9,20 +9,61 @@
                 >
                 <button 
                     class="btn btn-primary"
-                    @click="showPassword"
-                >{{showPasswordText}}</button>    
+                    @click="checkResultAge"
+                >Comprobar</button>    
             </div>
     
   </div>
+  <div>
+      <p>{{ result }}</p>
+    </div>
 </template>
 
 <script>
 export default {
   name: 'CheckAge',
-  props: {
-    
-  }
+  data(){
+    return{
+      result:"",
+      inputAge:"",
+    };
+  },
+  methods: {
+    checkResultAge(){
+    let inputAge = this.inputAge;
+    if (inputAge == "") {
+      this.result = "introduce tu edad"
+    }
+    else {
+      if(inputAge >= 18) {
+            this.result = "Puedes conducir";
+        }
+        else  {
+           
+          this.result = "No puedes conducir";
+        }
+    }    
+    },
+  }, 
 }
+
+/* methods: {
+    checkCanDrive() {
+      let age = this.ageInput;
+      if (age == "" || age.match("[a-zA-Z]+")) {
+        this.message = "Input inválido, escribe tu edad";
+        this.placeholder = "";
+      } else {
+        if (age >= 18) {
+          this.message = "Puede conducir";
+        } else {
+          this.message = "No Puede conducir";
+        }
+      }
+    },
+  },
+};
+ */
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
